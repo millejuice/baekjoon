5d2e3b625aaf04291dadb163fac37989efef7f5e
millejuice <potgusdavy@gmail.com>
2024-01-19 22:43:17 +0900

    시간복잡도 End

---
+++ 시간복잡도/24313.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a0,a1;
+    int c;
+    int n;
+    cin>>a1>>a0;
+    cin>>c;
+    cin>>n;
+
+    if(a1*n+a0<=c*n && a1<=c)
+        cout<<"1";
+    else
+        cout<<"0";
+}
