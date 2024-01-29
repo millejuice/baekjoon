2091dc333466adaa2dac2e8a83fc5a70644839a1
millejuice <potgusdavy@gmail.com>
2024-01-29 22:12:26 +0900

    pair,sort가지고 풀기

---
+++ 문제집공개automata/5635_pair_sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    int n;
+    cin>>n;
+    cin.ignore();
+    // 비교하기 쉽게 pair< pair<year,month>, pair<day,name>>> 순서로 pair 생성
+    vector<pair<pair<int,int>,pair<int,string>>> stu(n);
+
+    for(int i=0;i<n;i++){
+        cin >> stu[i].second.second >> stu[i].second.first >> stu[i].first.second >> stu[i].first.first;
+    }
+    sort(stu.begin(),stu.end());
+    cout<<stu[n-1].second.second<<"\n"<<stu[0].second.second<<endl;
+}
