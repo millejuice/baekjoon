03cf1e7fd0cefb477f6b119d8e9dcc4c65a02d51
millejuice <potgusdavy@gmail.com>
2025-07-21 11:07:12 +0900

    A86추가

--- pps/A86.java
+++ pps/A86.java
@@ -1,19 +1,59 @@
 package pps;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
 
 public class A86 {
-    public static void main(String[] args) throws NumberFormatException, IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        int range = m - n;
-        String[] s = new String[range+1];
-        String words[]={"zero","one","two","three","four","five","six","seven","eight","nine"};
-        for(int i = n; i<=m;i++){
-            
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+        String[] nums = {
+            "zero", "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine"
+        };
+
+        List<Pair> list = new ArrayList<>();
+
+        for (int i = M; i <= N; i++) {
+            String word;
+            if (i >= 10) {
+                int tens = i / 10;
+                int ones = i % 10;
+                word = nums[tens] + " " + nums[ones];
+            } else {
+                word = nums[i];
+            }
+            list.add(new Pair(word, i));
+        }
+
+        // 사전순 정렬
+        Collections.sort(list);
+
+        // 출력 (10개씩 한 줄)
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i).value + " ");
+            if (i % 10 == 9) {
+                System.out.println();
+            }
+        }
+    }
+
+    // 비교 기준: 문자열 기준으로 정렬
+    static class Pair implements Comparable<Pair> {
+        String word;
+        int value;
+
+        Pair(String word, int value) {
+            this.word = word;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return this.word.compareTo(o.word);
         }
     }
 }
+
